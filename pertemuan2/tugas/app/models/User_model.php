9debb766420e66e17bfa0d596308c9fe3551a180
azhar457 <azharsss457@gmail.com>
2025-11-10 00:43:19 +0700

    MVC Tugas Praktek pertemuan 2

--- pertemuan2/tugas/app/models/User_model.php
+++ pertemuan2/tugas/app/models/User_model.php
@@ -19,4 +19,41 @@ class User_model
         $this->db->bind('id', $id);
         return $this->db->single();
     }
+
+    public function tambahDataUser($data)
+    {    
+        $query = "INSERT INTO users (name, email) VALUES (:name, :email)";
+        $this->db->query($query);
+        $this->db->bind('name', $data['name']);
+        $this->db->bind('email', $data['email']);
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
+    public function hapusDataUser($id)
+    {
+        $query = "DELETE FROM users WHERE id = :id";
+        $this->db->query($query);
+        $this->db->bind('id', $id);
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
+    public function editDataUser($data)
+    {
+        // $data adalah $_POST, termasuk 'id'
+        $query = "UPDATE users SET 
+                    name = :name, 
+                    email = :email 
+                  WHERE id = :id";
+
+        $this->db->query($query);
+        $this->db->bind('name', $data['name']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('id', $data['id']); // Ambil id dari form
+
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
+
+    
+
 }
